3ebb198dc68e82403cb8d0f9d450874f9ad77c49
lbseven7 <leobarbosapinturas@gmail.com>
2022-09-28 10:01:22 -0300

    feat: Implementa a classe PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,18 @@
+import Battle from './Battle';
+import Fighter from '../Fighter';
+
+// criar a classe PVP
+export default class PVP extends Battle {
+  constructor(player: Fighter, private opponent: Fighter) {
+    super(player);
+  }
+  
+  fight(): number {
+    while (this.player.lifePoints > 0 && this.opponent.lifePoints > 0) {
+      this.player.attack(this.opponent);
+      this.opponent.attack(this.player);
+    }
+
+    return this.player.lifePoints > 0 ? 1 : -1;
+  }
+}
